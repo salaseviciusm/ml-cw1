56ffbdc4d7d497106cc9931deaec6f5fa9eaf79d
Devam Savjani <devamsavjani@rocketmail.com>
2021-10-31 12:24:54 +0000

    Impelemented decision_tree_learning

--- locator.py
+++ locator.py
@@ -6,29 +6,45 @@ data = np.loadtxt("wifi_db/clean_dataset.txt")
 num_of_labels = len(np.unique(data[:, -1]))
 label_col = 7
 
-
 def preprocess(dataset):
     dataset_deleted = np.delete(dataset, 0, -1)
     return dataset
 
 
 def decision_tree_learning(dataset, depth):
-    split = find_split(dataset)
-
+    if dataset.size == 0:
+        return None, depth
+    if len(np.unique(dataset[:, -1])) == 1:
+        return dict({"value" : np.unique(dataset[:, -1])[0]}), depth
+    else:
+        column_num, value = find_split(dataset)
+        print(column_num, value)
+        l_branch, l_depth = decision_tree_learning(dataset[dataset[:, column_num] < value, :], depth + 1)
+        r_branch, r_depth = decision_tree_learning(dataset[dataset[:, column_num] >= value, :], depth + 1)
+        node = {
+            "attribute" : column_num,
+            "value" : value,
+            "left" : l_branch,
+            "right" : r_branch,
+            "leaf" : False
+        }
+        return node, max(l_depth, r_depth)
+        
+# [[-59. -54. -58. -59. -63. -81. -80.   1.]
+#  [-60. -58. -57. -59. -66. -82. -83.   1.]
+#  [-60. -57. -58. -58. -50. -83. -88.   4.]]
 
 def find_split(dataset):
     print(dataset.shape)
     best_col_num = 0
     best_attr_index = 0
     best_attr_gain = 0
-
-    for col in range(0, len(dataset[0]) - 1):
+    for col in range(len(dataset[0]) - 1):
         attribute = np.sort(dataset[:, (col, label_col)], 0)
 
         maximum_information_gain = 0
         maximum_split_index = 0
         for split_index in range(1, len(dataset)):
-
             if attribute[split_index - 1][0] == attribute[split_index][0]:
                 # skips if split not possible i.e. attribute values are identical
                 continue
@@ -46,8 +62,8 @@ def find_split(dataset):
             best_attr_index = maximum_split_index
             best_col_num = col
 
-    return best_col_num, (dataset[best_attr_index][best_col_num] + dataset[best_attr_index - 1][best_col_num]) / 2, best_attr_gain
-
+    print(best_attr_gain)
+    return best_col_num, (dataset[best_attr_index][best_col_num] + dataset[best_attr_index - 1][best_col_num]) / 2
 
 def gain(dataset, leftset, rightset):
     return H(dataset) - remainder(leftset, rightset)
@@ -74,13 +90,16 @@ def remainder(left_set, right_set):
     return (left_size * H(left_set) / total_size) + (right_size * H(right_set) / total_size)
 
 
-H_total = H(data)  #  TODO:// inside gain function replace it with h_total
-active_data = data
-res = find_split(active_data)
-entropy = res[2]
-print(res)
-while entropy:
-    active_data = active_data[active_data[:, res[0]] > res[1], :]
-    res = find_split(active_data)
-    entropy = res[2]
-    print(res)
+# H_total = H(data)  #  TODO:// inside gain function replace it with h_total
+# active_data = data
+# res = find_split(active_data)
+# entropy = res[2]
+# print(res)
+# while entropy:
+#     active_data = active_data[active_data[:, res[0]] > res[1], :]
+#     res = find_split(active_data)
+#     entropy = res[2]
+#     print(res)
+
+final = decision_tree_learning(data, 0)
+print(final)
