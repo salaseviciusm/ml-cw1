28eda745fa935149a05d9e3c82838df377d77a50
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2021-11-02 13:05:20 +0000

    improve split finding speed - unsure of correctness of algo tho tbh with you mate

--- locator.py
+++ locator.py
@@ -1,39 +1,89 @@
 import math
 import numpy as np
-
-data = np.loadtxt("wifi_db/clean_dataset.txt")
-
-num_of_labels = len(np.unique(data[:, -1]))
-label_col = 7
+import sys
 
 
-def preprocess(dataset):
-    dataset_deleted = np.delete(dataset, 0, -1)
-    return dataset
+def read_dataset(path):
+    data = []
+    labels = []
+    label_freqs = {}
+    for line in open(path):
+        if line.strip() != "":
+            row = line.strip().replace("\t", " ").split(" ")
+            data.append(list(map(float, row[:-1])))
+
+            label = int(row[-1])
+            labels.append([label])
+
+            if label not in label_freqs:
+                label_freqs[label] = 0
+            label_freqs[label] += 1
+
+    data = np.array(data)
+    labels = np.array(labels)
+
+    data = np.append(data, labels, axis=1)
+
+    return data, label_freqs
+
+
+data, label_freqs = read_dataset("wifi_db/clean_dataset.txt")
+
+print(data)
 
 
 def decision_tree_learning(dataset, depth):
     split = find_split(dataset)
 
 
-def find_split(dataset):
+def find_split(dataset, freqs):
 
     best_col_num = 0
     best_attr_index = 0
     best_attr_gain = 0
 
+    np.set_printoptions(threshold=sys.maxsize)
+
     for col in range(0, len(dataset[0])-1):
-        attribute = np.sort(dataset[:, (col, label_col)], 0)
+        attribute = dataset[:, (col, 7)]
+        attribute = attribute[np.argsort(attribute[:, 0])]
+        print(attribute)
 
         maximum_information_gain = 0
         maximum_split_index = 0
 
-        for split_index in range(1, len(dataset)):
-            # TODO:// Handle dupplicate data for splits i.e. [56, 56, 56, 56].
-            data_l = attribute[:split_index]
-            data_r = attribute[split_index:]
+        split_index = 0
+        leftset_freqs = {elem: 0 for elem in freqs}
+        rightset_freqs = dict(freqs)
+        while split_index < len(dataset):
+            prev_val = None
+            split_val = None
 
-            info_gain = gain(dataset, data_l, data_r)
+            while split_index < len(attribute):
+                curr_val = attribute[split_index][0]
+
+                leftset_freqs[attribute[split_index][1]] += 1
+                rightset_freqs[attribute[split_index][1]] -= 1
+
+                split_index += 1
+                if prev_val is not None and curr_val != prev_val:
+                    split_val = (curr_val + prev_val) / 2
+                    break
+                prev_val = curr_val
+
+            if split_val is None:
+                break
+
+            data_l = attribute[attribute[:, 0] < split_val]
+            data_r = attribute[attribute[:, 0] >= split_val]
+
+            print(leftset_freqs)
+            print(rightset_freqs)
+            print(split_val)
+            print()
+
+            info_gain = gain(dataset, data_l, data_r,
+                             leftset_freqs, rightset_freqs)
             if info_gain > maximum_information_gain:
                 maximum_information_gain = info_gain
                 maximum_split_index = split_index
@@ -44,34 +94,42 @@ def find_split(dataset):
             best_col_num = col
 
     # TODO: do we need to get average value between best_attr_index and best_attr_index+1 ?
+    print(info_gain)
     return best_col_num, dataset[best_attr_index][best_col_num]
 
 
-def gain(dataset, leftset, rightset):
-    return H(dataset) - remainder(leftset, rightset)
+def gain(dataset, leftset, rightset, leftset_freqs, rightset_freqs):
+    freqs = {}
+    for elem in leftset_freqs:
+        freqs[elem] = leftset_freqs[elem]
+
+    for elem in rightset_freqs:
+        if elem not in freqs:
+            freqs[elem] = rightset_freqs[elem]
+        else:
+            freqs[elem] += rightset_freqs[elem]
+
+    return H(dataset, freqs) - remainder(leftset, rightset, leftset_freqs, rightset_freqs)
 
 
-def H(dataset):
+def H(dataset, freqs):
     assert dataset.shape[1] >= 2, dataset
     num_rows = dataset.shape[0]
-    freq = [0 for _ in range(num_of_labels)]
-    for row in dataset:
-        # todo: if relabelling need to change
-        freq[int(row[-1]) - 1] += 1
     entropy = 0
-    for elem in freq:
-        if elem != 0:
-            p_k = elem/num_rows
+    for elem in freqs:
+        if freqs[elem] != 0:
+            p_k = freqs[elem]/num_rows
             entropy -= p_k * math.log2(p_k)
     return entropy
 
 
-def remainder(left_set, right_set):
+def remainder(left_set, right_set, leftset_freqs, rightset_freqs):
     left_size = left_set.shape[0]
     right_size = right_set.shape[0]
     total_size = left_size + right_size
-    return (left_size * H(left_set) / total_size) + (right_size * H(right_set) / total_size)
+    return (left_size * H(left_set, leftset_freqs) / total_size) + (right_size * H(right_set, rightset_freqs) / total_size)
 
 
-H_total = H(data)  #  TODO:// inside gain function replace it with h_total
-print(find_split(data))
+#  TODO:// inside gain function replace it with h_total
+H_total = H(data, label_freqs)
+print(find_split(data, label_freqs))
